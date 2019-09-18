9156162af6114744e28e28663527201382d13c6c
VictorReyna <victor.reyna313@gmail.com>
2019-09-18 13:37:55 -0500

    Add files via upload

---
+++ 14.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+ 
+void Print_two_d(int two_d[3][4], int rows, int cols){ 
+    int i,j; 
+ 
+    for (i=0;i<rows;i++){ 
+        for (j=0;j<cols;j++){ 
+            printf("A[%d][%d]=%d ", i, j, two_d[i][j]); 
+        } 
+        printf("\n"); 
+    } 
+} 
+ 
+int main(void){ 
+ 
+    int two_d[3][4] = {{2,3,4,5},{3,4,5,6},{4,5,6,7}}; 
+ 
+    Print_two_d(two_d,3,4); 
+ 
+    return 0; 
+}
